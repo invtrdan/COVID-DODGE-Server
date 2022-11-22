8f8c768fe638e9cf4f5b1f8e2632ecff247321ca
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2022-11-22 22:43:25 +0000

    added leaderboard

---
+++ leaderboard.py
@@ -0,0 +1,15 @@
+from replit import db
+
+#Storing initial values in the database
+#Comment out after first run
+db["number1"] = "Dr.A"
+db["score1"] = 20
+db["number2"] = "Danielle"
+db["score2"] = 18
+db["number3"] = "Shemaya"
+db["score3"] = 17
+db["number4"] = "Antonia"
+db["score4"] = 10
+db["number5"] = "Brandon"
+db["score5"] = 3
+
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request
 from replit import db
+import leaderboard
 app = Flask('app')
+
+db["number1"] = "Dr.A"
+db["score1"] = 20
+db["number2"] = "Danielle"
+db["score2"] = 18
+db["number3"] = "Shemaya"
+db["score3"] = 17
+db["number4"] = "Antonia"
+db["score4"] = 10
+db["number5"] = "Brandon"
+db["score5"] = 3
 
 @app.route('/')
 def home():
@@ -18,4 +30,7 @@ def instructions():
 def play_game():
   return render_template("play_game.html")
 
+@app.route('/leaderboard')
+def display_leaderboard():
+  return render_template("leaderboard.html")
 app.run(host='0.0.0.0', port=81)
