ae0a07d8c6ab7bca10f7ceedd3bb97442c470bf0
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2022-11-22 23:24:13 +0000

    added link to homepage

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from replit import db
 import leaderboard
 app = Flask('app')
 
-db["number1"] = "Dr.A"
-db["score1"] = 20
-db["number2"] = "Danielle"
-db["score2"] = 18
-db["number3"] = "Shemaya"
-db["score3"] = 17
-db["number4"] = "Antonia"
-db["score4"] = 10
-db["number5"] = "Brandon"
-db["score5"] = 3
+# db["number1"] = "Dr.A"
+# db["score1"] = 20
+# db["number2"] = "Danielle"
+# db["score2"] = 18
+# db["number3"] = "Shemaya"
+# db["score3"] = 17
+# db["number4"] = "Antonia"
+# db["score4"] = 10
+# db["number5"] = "Brandon"
+# db["score5"] = 3
 
 @app.route('/')
 def home():
@@ -32,5 +32,14 @@ def play_game():
 
 @app.route('/leaderboard')
 def display_leaderboard():
-  return render_template("leaderboard.html")
+  number1 = db["number1"] + ": " + str(db["score1"])
+  number2 = db["number2"] + ": " + str(db["score2"])
+  number3 = db["number3"] + ": " + str(db["score3"])
+  number4 = db["number4"] + ": " + str(db["score4"])
+  number5 = db["number5"] + ": " + str(db["score5"])
+  return render_template('leaderboard.html', number1=number1, number2=number2, number3=number3, number4=number4, number5=number5)
+
+
+
+  
 app.run(host='0.0.0.0', port=81)
